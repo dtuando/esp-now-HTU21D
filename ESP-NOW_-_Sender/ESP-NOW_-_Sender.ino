8778f67d29201bbe80f93308df2ffdfe35f377b4
dtuando <dtuando@gmail.com>
2022-03-23 22:28:46 -0700

    Add files via upload

---
+++ ESP-NOW_-_Sender/ESP-NOW_-_Sender.ino
@@ -0,0 +1,94 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Adafruit_HTU21DF.h>
+#include <Wire.h>
+
+Adafruit_HTU21DF htu;
+
+// REPLACE WITH YOUR RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0x24, 0x0A, 0xC4, 0xEE, 0x79, 0x1C};
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+  float a;
+  float b;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+ // Sensor Check
+ if (!htu.begin()) {
+    Serial.println("Couldn't find sensor!");
+    while (1);
+  }
+}
+ 
+void loop() {
+   // Get Temp & Humidity
+    float temp = (htu.readTemperature()* 1.8) + 32;
+    float rel_hum = htu.readHumidity();
+    delay(500);
+  // Set values to send
+  myData.a = temp;
+  myData.b = rel_hum;
+  
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(2000);
+}
